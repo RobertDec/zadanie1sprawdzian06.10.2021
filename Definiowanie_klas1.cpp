a367915710e634e500d886e8c0f9530125b35ba7
Robert <87870041+RobertDec@users.noreply.github.com>
2021-10-06 14:00:45 +0200

    Add files via upload

---
+++ Definiowanie_klas1.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Triangle {
+public:
+	int bok_a, bok_b, bok_c, h;
+
+	float obliczPole() {
+		int pole;
+		pole = bok_b * h;
+		return pole;
+	}
+	int obliczObwod() {
+		int obw;
+		obw = bok_a + bok_b + bok_c;
+		return obw;
+	}
+};
+
+int main()
+{
+	Triangle trojkat;
+	trojkat.bok_a = 10;
+	trojkat.bok_b = 11;
+	trojkat.bok_c = 12;
+	trojkat.h = 7;
+	
+	
+	cout <<"Pole wynosi: " << trojkat.obliczPole() << endl;
+	cout <<"Obwod wynosi: " << trojkat.obliczObwod() << endl;
+}
+
+
+
